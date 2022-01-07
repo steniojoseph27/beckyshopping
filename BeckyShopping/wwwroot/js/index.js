db78369c8116d9efc4ff168bfb5282234ad01860
Stenio Joseph <steniojoseph@yahoo.com>
2022-01-07 01:33:03 -0500

    -- programmed buy button using javascript

---
+++ BeckyShopping/wwwroot/js/index.js
@@ -0,0 +1,15 @@
+﻿var x = 0;
+var s = "";
+
+console.log("Welcome to Becky Shopping");
+
+var theForm = document.getElementById("theForm");
+theForm.hidden = true;
+
+var button = document.getElementById("buyButton");
+button.addEventListener("click", function () {
+    console.log("Buying item");
+});
+
+var productinfo = document.getElementsByClassName("product-props");
+var listItems = productInfo.item[0].children;
