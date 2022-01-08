5a48b1a58994d81a33d5013acd68b9dc1568989e
Stenio Joseph <steniojoseph@yahoo.com>
2022-01-08 17:34:26 -0500

    -- added boostrap - fontawesome - added data/entities, defined: order, orderitem, product, storeuser models

---
+++ BeckyShopping/Data/Entities/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DutchTreat.Data.Entities
+{
+  public class Product
+  {
+    public int Id { get; set; }
+    public string Category { get; set; }
+    public string Size { get; set; }
+    public decimal Price { get; set; }
+    public string Name { get; set; }
+    public string ProductDescription { get; set; }
+    public string SupplierId { get; set; }
+    public string Supplier { get; set; }
+    public DateTime ReleaseDate { get; set; }
+    public DateTime ExpirationDate { get; set; }
+    public string ProductOrigin { get; set; }
+  }
+}
